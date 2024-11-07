45ea4837635ac0afed609f3ca6885aeab5fe6c22
70928manson <manson972@yahoo.com>
2024-11-07 15:59:54 +0800

    feat: add autocomplete component

---
+++ autocomplete/autocomplete.js
@@ -0,0 +1,38 @@
+let availableOptions = [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "React",
+    "Redux",
+    "Next.js",
+    "Typescript",
+    "Next.js",
+    "LOL",
+    "SKT T1 Faker"
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+// !!注意!! 這邊用 onkeyup 的話, 若輸入法為中文會觸發不了
+inputBox.oninput = function() {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length > 0) {
+        result = availableOptions.filter((option) => option.toLowerCase().includes(input.toLowerCase()));
+    };
+    display(result);
+};
+
+function display(result) {
+    let content = "";
+    for (const list of result) {
+        content += `<li onclick=selectInput(this)>${list}</li>`;
+    }
+    resultsBox.innerHTML = result.length > 0 ? `<ul>${content}</ul>` : "";
+};
+
+function selectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = "";
+}
